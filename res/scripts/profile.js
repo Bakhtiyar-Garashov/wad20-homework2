aec48859cd8bbd56bf35b770fd0aa045a94129cf
bakhtiyar_garashov <bakhtiyar.garashov@ut.ee>
2020-10-23 16:06:15 +0300

    last touches before submit

--- res/scripts/profile.js
+++ res/scripts/profile.js
@@ -2,23 +2,23 @@
 // TASK 1. Fetch user profile information from users endpoint
 
 $(function () {
-	
+
 	let url = "https://private-anon-454dca17cb-wad20postit.apiary-mock.com/users/1";
 
 	$.get(url, function (response) {
 		let name = response.firstname + " " + response.lastname;
 		let email = response.email;
-		
+
 		let avatarurl = response.avatar;
 		$('.avatar').attr('src', avatarurl);
-		
+
 		$('#name').text(name);
 		$('#email').text(email);
 	});
 
-	$('.avatar').click(function() {
+	$('.avatar').click(function () {
 		$('.drop-down-menu').toggle();
 	});
-	
-	
+
+
 })
